45b6b0e97512e9ed1fabe6c422a586d669fa8ef5
t0msh <78264628+t0msh@users.noreply.github.com>
2021-01-30 14:49:09 +0000

    is this working?

---
+++ usbprep.ps1
@@ -0,0 +1,67 @@
+$counter = 0
+$usbdrive = Get-WmiObject win32_diskdrive | Where-Object {$_.interfacetype -like 'USB'}
+$drivenum = $usbdrive | Measure-Object
+$drivecount = $drivenum.Count
+
+if ($drivecount -lt 1)
+{
+    Write-Host "No USB drive detected! Please connect the drive(s) you wish to prepare for BitLocker encryption and retry." -ForegroundColor Red
+}
+    else 
+    {
+        Write-Host "There is currently $drivecount drive(s) connected ready for preperation"
+        Start-Sleep -Seconds 1
+            foreach($usbdrive in $usbdrive)
+            {
+                $diskID = $usbdrive.DeviceID.Substring(17,1).trim()
+                $disksize = (($usbdrive.Size)/1GB).ToString(".00")+"GB"
+                $diskmodel = $usbdrive.Model 
+                $diskprep = (diskpart.exe /s disk.txt)
+                Write-Host "CAUTION! Are you sure you want to prepare $diskmodel ($disksize) for BitLocker?" -ForegroundColor Yellow -BackgroundColor Red
+                $confirmation = Read-Host "( Y / N )"
+                switch ($confirmation) 
+                {
+                    Y 
+                    {
+                        Clear-Host
+                        Write-Host "Preparing drive $diskmodel ($disksize) for BitLocker." -ForegroundColor Green; $prep = $true
+                        Start-Sleep -Seconds 1 
+                    }
+                    N 
+                    {
+                        Clear-Host
+                        Write-Host "No, leave $diskmodel ($disksize) alone." -ForegroundColor Red; $prep = $false
+                        Start-Sleep -Seconds 1
+                    }  
+                    Default 
+                    {
+                        Clear-Host
+                        Write-Host "No, leave $diskmodel ($disksize) alone." -ForegroundColor Red; $prep = $false
+                        Start-Sleep -Seconds 1
+                    }
+                }
+                if ($prep = $true)
+                {
+                    New-Item -Name disk,txt -ItemType file -Force | Out-Null
+                    New-Item -Path disk.txt "SEL DISK $diskid"
+                    New-Item -Path disk.txt "SEL PART 1"
+                    New-Item -Path disk.txt "INACTIVE"
+                    $diskprep | Out-Null
+                    Write-Host "Drive $diskmodel ($disksize) is now ready for BitLocker encryption!" -ForegroundColor Green
+                    Remove-Item -path disk.txt -force
+                    ++$counter
+                }
+                else 
+                {
+                    Write-Host "Drive $diskmodel ($disksize) has not been modified. BitLocker encryption may fail if there are any active MBR partitions on the disk" -ForegroundColor Red
+                }
+            }
+    }
+    if ($counter -gt 0)
+    {
+        Write-Host "$counter USB drive(s) have been prepared for BitLocker Encryption! Nice!" -ForegroundColor Green
+    }
+    else 
+    {
+       Write-Host "No USB drives have been prepared for BitLocker. Exiting" -ForegroundColor Red 
+    }
